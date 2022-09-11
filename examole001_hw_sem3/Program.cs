1a5621bfc2ca60628ad0c570e7188b0de4e1a70b
Prometheus <poligraphich@gmail.com>
2022-09-11 20:59:11 +0300

    add exam001_sem3

---
+++ examole001_hw_sem3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number;
+int rev = 0;
+bool FindPolindrom(int num)
+{
+    while (temp != 0)
+    {
+        rev = (rev * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+    return rev == number;
+}
+if (FindPolindrom(temp) == true)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
