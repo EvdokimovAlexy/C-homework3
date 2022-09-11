70d6f7c508b8663b3d16b339def21b28b7cb8cdb
Prometheus <poligraphich@gmail.com>
2022-09-11 23:15:40 +0300

    add exam003_sem3

---
+++ examole003_hw_sem3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int Prompt(string message)
+{
+    Console.Write(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+int GivesCube(int number)
+{
+    int num = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine(i + "\t" + i * i * i);
+    }
+    return num;
+}
+int number = Prompt("Введите число > ");
+GivesCube(number);
